77074d7435359206e0cb5321d63e480cc86c9a9b
nikosNalmpantis <nikosnalmpantis@outlook.com>
2021-07-29 13:45:49 +0300

    feat: add more documentation

--- app/obqna/process.py
+++ app/obqna/process.py
@@ -4,7 +4,6 @@ from typing import List, Dict
 import nltk.data
 import pandas as pd
 import re
-import tika
 from gensim.parsing.preprocessing import strip_multiple_whitespaces
 from nltk.tokenize.punkt import PunktSentenceTokenizer
 from pandarallel import pandarallel
@@ -16,6 +15,11 @@ class PDFParser:
         self.directory = books_directory
 
     def parse(self) -> List[str]:
+        """ Parses the raw pdf(s) provided in the books_directory
+
+        :return: A list wiht the parsed text of the pdf(s)
+        :rtype: List[str]
+        """        
         corpus = []
         for book in os.listdir(self.directory):
             temp = tikaparser.from_file(self.directory+book)["content"]
@@ -24,6 +28,13 @@ class PDFParser:
         return corpus
 
     def clean(self, corpus: List[str]) -> pd.DataFrame:
+        """ Applies basic text cleaning.
+
+        :param corpus: A list wiht the parsed text of the pdf(s)
+        :type corpus: List[str]
+        :return: A DataFrame with a single column named "text" containing the cleaned input
+        :rtype: pd.DataFrame
+        """        
         corpus = [strip_multiple_whitespaces(n) for n in corpus]
         corpus = [n.encode("ascii", "ignore").decode() for n in corpus]
         corpus = pd.DataFrame({"text": corpus})
@@ -44,6 +55,13 @@ class Passages:
         pandarallel.initialize(nb_workers=self.nb_workers)
 
     def chunker(self, text: str) -> List[List[str]]:
+        """ Brakes a text into passages for parallelization
+
+        :param text: A single text
+        :type text: str
+        :return: A List containing chunks of the input text, where chunks are lists of sentences
+        :rtype: List[List[str]]
+        """        
         for pat in self.patterns:
             text = pat.sub("", str(text))
         text = text.encode("ascii", "ignore").decode()
@@ -55,6 +73,17 @@ class Passages:
         return chunks
 
     def combine(self, data: List[str], lim: int = 60, upper_lim: int = None) -> List[str]:
+        """ Combines A list of sentences to passages of approximate num of words close to `lim`
+
+        :param data: A list containing sentences
+        :type data: List[str]
+        :param lim: The lim that num of words should approximate, defaults to 60
+        :type lim: int, optional
+        :param upper_lim: The max lim that num of words should not exceed, defaults to None
+        :type upper_lim: int, optional
+        :return: List of passages
+        :rtype: List[str]
+        """        
         data = {s: len(self.pattern_find.findall(s)) for s in data}
         upper_lim = upper_lim or int(lim*1.2)
         passages = []
@@ -88,13 +117,12 @@ class Passages:
         return passages
 
     def df2passages(self, df: pd.DataFrame) -> List[Dict[str, str]]:
-        """ Transforms DataFrame with content to list of dictionary with the content in passages.
+        """ Transforms DataFrame containing text(s) to a list of dicts containing passages of the text(s)
 
-        :param df: DataFrame containing all the books.
+        :param df: DataFrame containing all the text(s)
         :type df: pd.DataFrame
-
-        :return: List of Dicts
-        :rtype: List
+        :return: List of dicts containing passages of the text(s)
+        :rtype: List[Dict[str, str]]
         """
         df["text"] = df["text"].parallel_apply(self.chunker)
         df = df.explode("text").reset_index(drop=True)
